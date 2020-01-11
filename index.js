24d81e7dc4f74313fe823ea32958172fc808f25f
Raul Calvo <raulcalvo@gmail.com>
2020-01-11 12:17:32 +0100

    Evolution

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
 'use strict';
+global.__basedir = __dirname;
 
-const expressFileConsumer = require("express-file-consumer");
-const logger = require("logger-to-memory");
+const requirer = require("../extended-requirer/index.js");
+const r = new requirer(__dirname,{"currentConfig" : "DEV"});
+
+const expressFileConsumer = r.require("express-file-consumer");
+const logger = r.require("logger-to-memory");
+
 const { createWorker } = require('tesseract.js');
-const path = require("path");
-const fs = require("fs");
+const path = r.require("path");
+const fs = r.require("fs");
 
 var loggerConfig = {
-    "logsEnabled": true,
-    "maxLogLines": 20,
-    "logToConsole": true,
-    "lineSeparator": "<br>"
+    "logger-to-memory" :{
+        "logsEnabled": true,
+        "maxLogLines": 20,
+        "logToConsole": true,
+        "lineSeparator": "<br>"
+    }
 };
 var log = new logger(loggerConfig);
 
-var config = {
-    consumer: {
-        "inputFolder": "./data/input",
-        "outputFolder": "./data/output",
-        "watch": true,
-        "afterProcessPolicy": 2,
-        "processFunction": processFile
-    },
-    "logger": log
-};
-
-function getOutputFileWithNoExtension(file) {
-    const fileName = path.basename(file, path.extname(file));
-    const outFile = config.consumer.outputFolder + "/" + fileName;
-    return outFile;
-}
-
-function writeResult(file, text){
-    log.log(text);
-    const outputFile =getOutputFileWithNoExtension(file) + ".txt";
+function writeResult(file, outputFolder, text){
+    const outputFile = path.join(outputFolder, path.basename(file, path.extname(file)) + ".txt");
     fs.writeFileSync( outputFile, text );
     return Promise.resolve("File " + file + " processed and result saved to " + outputFile);
 }
 
-function processFile(file) {
+function processFile(file, outputFolder) {
     const worker = createWorker();
     return (async () => {
       await worker.load();
@@ -47,9 +36,18 @@ function processFile(file) {
       const { data: { text } } = await worker.recognize(file);
 
       await worker.terminate();
-      return await writeResult(file, text);
+      return await writeResult(file, outputFolder, text);
     })();
 }
-
+var config = {
+    "file-consumer": {
+        "inputFolder": "./data/input",
+        "outputFolder": "./data/output",
+        "watch": true,
+        "afterProcessPolicy": 2,
+        "processFunction": processFile
+    }
+};
 var _expressFileConsumer = new expressFileConsumer(config);
+_expressFileConsumer.setLogger(log);
 _expressFileConsumer.startListening();
